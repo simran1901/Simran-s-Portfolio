29417f74fc97a9bfc32e42e16538450ee23236d7
simran1901 <simranmakhijani55@gmail.com>
2023-07-04 16:56:38 +0530

    updated resume url

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,6 +14,6 @@ const linkedin = 'https://www.linkedin.com/in/simran-makhijani-428871123/';
 const github = 'https://github.com/simran1901/';
 const twitter = 'https://twitter.com/SimranMakhijan6/';
 const coding = 'https://clist.by/coder/simranmakhijani55/';
-const resume = 'https://shorturl.at/otX36';
+const resume = 'https://shorturl.at/fiCHY';
 
 // assets: https://shorturl.at/dmrt9
